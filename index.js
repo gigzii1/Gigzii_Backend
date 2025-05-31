aca855771ca6a4d29eb6ef96ebbbfda40278e305
OptimaCoders <167647381+optimacoders@users.noreply.github.com>
2025-05-31 12:45:36 +0530

    verce json added

--- index.js
+++ index.js
@@ -27,10 +27,6 @@ app.use(cors({
   credentials: true,
 }));
 
-app.use((req, res, next) => {
-  console.log("Request Origin:", req.headers.origin);
-  next();
-});
 
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ limit: '100mb', extended: true }));
